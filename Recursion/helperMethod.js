b96095a8db2998d333cedae5fee8eb3802a7bff4
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-03-15 09:14:13 +0200

    Add files via upload

---
+++ Recursion/helperMethod.js
@@ -0,0 +1,17 @@
+function solve(arr){
+    let result = [];
+
+    function helper(input){
+        if (input.length === 0){
+            return
+        }
+        if (input[0] % 2 !== 0){
+            result.push(input[0])
+        }
+        helper(input.slice(1))
+    }
+    helper(arr)
+    return result;
+}
+
+solve([1, 2, 3, 4, 5, 6, 7, 8, 9])
